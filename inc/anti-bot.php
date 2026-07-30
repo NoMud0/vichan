27b63abfeed64e73141e8addd210ebcbdec7581d
NoMud0 <53879478+NoMud0@users.noreply.github.com>
2026-07-30 16:36:59 +0200

    composer lock update and fix, remove unused file

--- inc/anti-bot.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-/*
- *  Anti-bot.php has been deprecated and removed due to its functions not being necessary and being easily bypassable, by both customized and uncustomized spambots.
- */
